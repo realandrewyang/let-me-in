ee8939019500ef038a6bb5d59c5a287304e12532
Andrew Yang <andrew.yang.school@gmail.com>
2019-11-23 21:28:10 -0500

    add logic for state - added temp file to store state - only send notification if state has changed and desired state is caught

--- check_load_test.py
+++ check_load_test.py
@@ -10,7 +10,22 @@ TEST_COURSE_NAME = "CO 250"
 TEST_SUBJECT = "CO"
 TEST_NUMBER = 250
 TEST_STATE = OPEN
+TEST_STATUS_FILE_PATH = "./test_data/state.txt"
 VALID_RESULT = {"SendMessageWithReferenceResult": "Message queued successfully"}
+
+# Reads state from file and returns as an integer
+def check_prev_status(local_file_path):
+    temp = -1
+    f = open(local_file_path, "r")
+    f.read(temp)
+    f.close()
+    return int(temp)
+
+# Update status to file
+def write_status(local_file_path, status):
+    f = open(local_file_path, "w")
+    f.write(str(status))
+    f.close()
 
 def run_test():
 
@@ -27,6 +42,12 @@ def run_test():
         ),
         TEST_COURSE_ID
     )
+
+    # Don't send notification if status hasn't changed
+    if status[0] == check_prev_status(TEST_STATUS_FILE_PATH): return
+
+    # Update state
+    write_status(TEST_STATUS_FILE_PATH, status[0])
     
     # Check whether to send or not
     if status[0] == TEST_STATE:
